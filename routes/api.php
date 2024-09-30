a7e83e155175afe96dc99ecc19e4c07fd276f642
Kiyoonewton <Kiyoonewton41@gmail.com>
2024-09-30 20:31:35 +0100

    Feat: add auth

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use App\Http\Controllers\Api\V1\CustomersController;
 use App\Http\Controllers\Api\V1\InvoicesController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -9,7 +11,13 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function(){
+Route::group(['prefix' => 'v1', 'namespace' => 'App\Http\Controllers\Api\V1'], function () {
     Route::apiResource('customers', CustomersController::class);
     Route::apiResource('invoices', InvoicesController::class);
 });
+
+Route::post("/login", [AuthController::class, 'login']);
+Route::post("/logout", [AuthController::class, 'logout']);
+Route::post("/register", [AuthController::class, 'register']);
+
+Route::resource("/task", TaskController::class);
